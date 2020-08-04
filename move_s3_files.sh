73cac6a6c1166ce5c71a3bb5f380e2be9bd4cda1
rafael.klock <rafael.klock@softplan.com.br>
2020-08-04 10:51:17 -0300

    update

--- move_s3_files.sh
+++ move_s3_files.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 #
-# DataTeam 03-08-2020
+# 03-08-2020
 # Mover arquivos para .pdf no s3
+# Rafael Klock
 #
 
-PROFILE='DT-DEV'
+PROFILE=''
 S3_BUCKET='my_bucket'
-S3_DIR='tjpe/2020/07/31'
+S3_DIR='meudir/sub_dir/01/03/teste'
+
 
 LISTA=$(aws --profile ${PROFILE} s3 ls s3://${S3_BUCKET}/${S3_DIR}/|awk '{print $4}' |grep -v pdf|grep -v 'json.gz' | sort -r)
 
 for arquivo in ${LISTA}; do
         echo "movendo arquivo: ${arquivo} para ${arquivo}.pdf"
-        aws --profile ${PROFILE} s3 mv s3://${S3_BUCKET}/${S3_DIR}/${arquivo} s3://${S3_BUCKET}/${S3_DIR}/${arquivo}.pdf &
+        echo aws --profile ${PROFILE} s3 mv s3://${S3_BUCKET}/${S3_DIR}/${arquivo} s3://${S3_BUCKET}/${S3_DIR}/${arquivo}.pdf &
         sleep 1.5
 
 done
